3dfb20aba63389f645e9b86e473ad660e0585591
dxf <2812964847@qq.com>
2024-07-02 04:07:31 +0800

    初始配置完成的项目

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    aappp
+    <el-input></el-input>
+    <el-button type="primary">按钮</el-button>
+  </div>
+  <RouterView />
+</template>
+
+<style scoped></style>
